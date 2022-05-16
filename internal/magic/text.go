c68cdf2cf79082e8d74afb3ead508c0f9447610a
jabbahotep <mike@td-systems.com>
2022-05-16 14:47:16 +0300

    Changed structure

--- internal/magic/text.go
+++ internal/magic/text.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gabriel-vasile/mimetype/internal/charset"
-	"github.com/gabriel-vasile/mimetype/internal/json"
+	"github.com/Diggernaut/mimetype/internal/charset"
+	"github.com/Diggernaut/mimetype/internal/json"
 )
 
 var (
